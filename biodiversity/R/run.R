2503f9b7a0a4c28a00ac40a7ff7bbdbd6e0190d7
Anders Nielsen <fishfollower@gmail.com>
2018-03-22 15:42:13 +0100

    major step forward

--- biodiversity/R/run.R
+++ biodiversity/R/run.R
@@ -13,30 +13,97 @@
 ##' data(species)
 ##' fit <- biodiv(species, conf=1)
 biodiv <- function(data, conf, run=TRUE, ...){
-  dat<-data[(data$density*data$survar)>1.0e-6,]
+  dat<-data[(data$density*data$asurv)>1.0e-6,]
   data <- list()
-  data$lat <- dat$lat
-  data$nsp <- dat$nsp
-  data$abund <- dat$density*dat$survar
-  data$logsiz <- dat$siz
-  data$asampl <- dat$asampl
-  data$mesh <- dat$mesh
-  data$temp <- (dat$sst+273.15)*8.62/100000
-  data$npp <- dat$npp
-  data$cat <- as.integer(dat$mlgr)-1
-  param <- list()
-  param$b5<- rep(-.5,length(unique(data$cat)))
-  param$b0 <- 20
-  param$b1 <- -0.1
-  param$b2 <- 0.4
-  param$logb3 <- 1
-  param$b4 <- 0.2
-  param$logk<-3
-  param$dummy<-0
+  data$code <- conf
+  if(data$code==1){
+    data$lat <- dat$lat
+    data$nsp <- dat$nsp
+    data$abund <- dat$density*dat$asurv
+    data$logsiz <- dat$siz
+    data$asampl <- dat$aswept
+    data$mesh <- dat$mesh
+    data$temp <- (dat$sst+273.15)*8.62/100000
+    data$npp <- dat$npp
+    data$cat <- as.integer(dat$mlgr)-1
+  }
+  if(data$code==2){
+    data$nsp <- dat$nsp
+  }
+  if(data$code==3){
+    data$lat <- dat$lat
+    data$lon <- dat$lon+100
+    data$nsp <- dat$nsp
+    data$abund <- dat$density*dat$asurv
+    data$siz <- dat$siz
+    data$asampl <- dat$aswept
+    data$mesh <- dat$mesh
+    data$depth <- dat$depth
+    data$npp <- dat$npp
+    data$cat <- as.integer(dat$mlgr)-1
+  }
+  if(data$code==4){
+    data$nsp <- dat$nsp
+    data$density <- dat$density
+    data$siz <- dat$siz
+    data$asampl <- dat$aswept
+    data$mesh <- dat$mesh
+    data$temp <- (dat$sst+273.15)*8.62/100000
+    data$npp <- dat$npp
+    data$cat <- as.integer(dat$mlgr)-1
+  }
 
-  low <- c(-10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -Inf, -Inf, -Inf, -100, 0, -100)
-  hig <- c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, Inf, Inf, Inf, 100, Inf, 100)
+  param <- list()
+  if(data$code==1){
+    param$b5<- rep(-.5,length(unique(data$cat)))
+    param$b0 <- 20
+    param$b1 <- -0.1
+    param$b2 <- 0.4
+    param$logb3 <- 1
+    param$b4 <- 0.2
+    param$logk<-3
+    param$dummy<-0
+  }
+  if(data$code==2){
+    param$logb5<- rep(-1,length(data$nsp))
+  }
+  if(data$code==3){
+    param$loga7 <- -2
+    param$b5<- rep(0,length(unique(data$cat)))
+    param$b0 <- 0
+    param$b1 <- 0
+    param$b2 <- 0
+    param$b4 <- 0.1
+    param$logk<-0
+  }
+  if(data$code==4){
+    param$loga7 <- 16
+    param$b5<- rep(0,length(unique(data$cat)))
+    param$logb1 <- -1
+    param$logb2 <- -1
+    param$logb3 <- -1
+    param$logb4 <- 0.1
+    param$logk<-1
+  }
+  
   obj <- MakeADFun(data, param, DLL="biodiversity", silent=TRUE)
+  
+  low <- rep(-Inf,length(obj$par))
+  hig <- rep(Inf,length(obj$par))
+  if(data$code==1){
+    low <- c(-10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -Inf, -Inf, -Inf, -100, 0, -100)
+    hig <- c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, Inf, Inf, Inf, 100, Inf, 100)
+  }
+  if(data$code==2){
+  }
+  if(data$code==3){
+    low <- c(-Inf,-10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10,-Inf, -Inf, -1, -100)
+    hig <- c(Inf, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, Inf, Inf, 1, 100)
+  }
+  if(data$code==4){
+    low <- c(0, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10,-10, -Inf, -10, -100)
+    hig <- c(Inf, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 2, 10, 10, 10, 100)
+  }
   opt <- nlminb(obj$par, obj$fn, obj$gr, lower=low, upper=hig, control=list(eval.max=10000, iter.max=10000))
   rep <- obj$report()
   sdrep <- sdreport(obj,opt$par)
--- biodiversity/src/biodiversity.cpp
+++ biodiversity/src/biodiversity.cpp
@@ -3,62 +3,196 @@
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
-  DATA_VECTOR(lat);
-  DATA_VECTOR(nsp);
-  DATA_VECTOR_INDICATOR(keep, nsp);
-  DATA_VECTOR(abund);
-  DATA_VECTOR(logsiz);
-  DATA_VECTOR(asampl);
-  DATA_VECTOR(temp);
-  DATA_IVECTOR(cat);
-  DATA_VECTOR(npp);
-  DATA_VECTOR(mesh);  
+  DATA_INTEGER(code)
+  Type nll=0;
+  if(code==1){
+    DATA_VECTOR(lat);
+    DATA_VECTOR(nsp);
+    DATA_VECTOR_INDICATOR(keep, nsp);
+    DATA_VECTOR(abund);
+    DATA_VECTOR(logsiz);
+    DATA_VECTOR(asampl);
+    DATA_VECTOR(temp);
+    DATA_IVECTOR(cat);
+    DATA_VECTOR(npp);
+    DATA_VECTOR(mesh);  
   
-  PARAMETER_VECTOR(b5);
-  PARAMETER(b0);
-  PARAMETER(b1);
-  PARAMETER(b2);
-  PARAMETER(logb3);
-  PARAMETER(b4);
-  PARAMETER(logk);
-  vector<Type> a=exp(b0+b1*logsiz*logsiz);
-  vector<Type> nu=exp(-b2/temp);
-  Type b3=exp(logb3);
-  Type k=exp(logk);
+    PARAMETER_VECTOR(b5);
+    PARAMETER(b0);
+    PARAMETER(b1);
+    PARAMETER(b2);
+    PARAMETER(logb3);
+    PARAMETER(b4);
+    PARAMETER(logk);
+    vector<Type> a=exp(b0+b1*logsiz*logsiz);
+    vector<Type> nu=exp(-b2/temp);
+    Type b3=exp(logb3);
+    Type k=exp(logk);
   
-  int nobs=nsp.size();
-  vector<Type> mu(nobs);
+    int nobs=nsp.size();
+    vector<Type> mu(nobs);
   
-  for(int i=0; i<nobs; ++i){
+    for(int i=0; i<nobs; ++i){
       mu(i)=a(i)*nu(i)*
         log(Type(1)-(b3*logb3/(1-b3))*(abund(i))/(a(i)*nu(i)))*
 	exp(b4*log(asampl(i)))*exp(b5(cat(i))*log(mesh(i)));
-      }
+    }
   
-  vector<Type> var = mu + mu*mu/k;
-  Type nll=0;
-  for(int i=0; i<nobs; ++i){
-    nll += -keep(i)*dnbinom2(nsp(i),mu(i),var(i),true);
+    vector<Type> var = mu + mu*mu/k;
+    for(int i=0; i<nobs; ++i){
+      nll += -keep(i)*dnbinom2(nsp(i),mu(i),var(i),true);
+    }
+  
+    REPORT(mu);
+    REPORT(var);
+    REPORT(b0);
+    REPORT(b1);
+    REPORT(b2);
+    REPORT(b3);
+    REPORT(b4);
+    REPORT(b5);
+    REPORT(k);    
+    ADREPORT(b0);
+    ADREPORT(b1);
+    ADREPORT(b2);
+    ADREPORT(b3);
+    ADREPORT(b4);
+    ADREPORT(b5);
+    ADREPORT(k);
+    REPORT(nll);
   }
-  
-  REPORT(mu);
-  REPORT(var);
-  REPORT(b0);
-  REPORT(b1);
-  REPORT(b2);
-  REPORT(b3);
-  REPORT(b4);
-  REPORT(b5);
-  REPORT(k);
+  if(code==2){
+    DATA_VECTOR(nsp);
+    DATA_VECTOR_INDICATOR(keep, nsp);
+    PARAMETER_VECTOR(logb5);
+    vector<Type> b5=exp(logb5);
+    Type k=37.24;
+    int nobs=nsp.size();
+    vector<Type> mu(nobs);  
+    for(int i=0; i<nobs; ++i){
+      mu(i)=b5(i)+Type(1.0e-12);
+    }
+    vector<Type> var = mu + mu*mu/k;
+    vector<Type> logL= dnbinom2(nsp,mu,var,true);
+    nll = -sum(vector<Type>(keep*logL));
+    REPORT(mu);
+    REPORT(b5);
+    REPORT(k);   
+    ADREPORT(b5);
+    REPORT(nll);
+    REPORT(logL);
+  }
+  if(code==3){
+    DATA_VECTOR(lat);
+    DATA_VECTOR(lon);
+    DATA_VECTOR(nsp);
+    DATA_VECTOR_INDICATOR(keep, nsp);
+    DATA_VECTOR(abund);
+    DATA_VECTOR(siz);
+    DATA_VECTOR(asampl);
+    DATA_VECTOR(depth);
+    DATA_IVECTOR(cat);
+    DATA_VECTOR(npp);
+    DATA_VECTOR(mesh);  
     
-  ADREPORT(b0);
-  ADREPORT(b1);
-  ADREPORT(b2);
-  ADREPORT(b3);
-  ADREPORT(b4);
-  ADREPORT(b5);
-  ADREPORT(k);
-  REPORT(nll);
+    PARAMETER(loga7);
+    PARAMETER_VECTOR(b5);
+    PARAMETER(b0);
+    PARAMETER(b1);
+    PARAMETER(b2);
+    PARAMETER(b4);
+    PARAMETER(logk);
+
+    vector<Type> loga=(siz+b1*siz*siz);
+    vector<Type> a=exp(loga);
+    Type a7=exp(loga7);
+    Type k=exp(logk);
+    int nobs=nsp.size();
+    vector<Type> mu(nobs);
+    for(int i=0; i<nobs; ++i){
+      mu(i)=a7*a(i)*
+        pow(lat(i), b2)*pow(lon(i), b0)*
+        pow(asampl(i),b4)*exp(b5(cat(i))*log(mesh(i)))+Type(1.0e-12);
+    }
+    vector<Type> var = mu + mu*mu/(k+Type(1.0e-12));
+    vector<Type> logL= dnbinom2(nsp,mu,var,true);
+    nll = -sum(vector<Type>(keep*logL));  
+    REPORT(mu);
+    REPORT(var);
+    REPORT(a7);
+    REPORT(b0);
+    REPORT(b1);
+    REPORT(b2);
+    REPORT(b4);
+    REPORT(b5);
+    REPORT(k);    
+    ADREPORT(a7);
+    ADREPORT(b0);
+    ADREPORT(b1);
+    ADREPORT(b2);
+    ADREPORT(b4);
+    ADREPORT(b5);
+    ADREPORT(k);
+    REPORT(nll);
+    REPORT(logL);
+  }
+  if(code==4){
+    DATA_VECTOR(nsp);
+    DATA_VECTOR_INDICATOR(keep, nsp);
+    DATA_VECTOR(density);
+    DATA_VECTOR(siz);
+    DATA_VECTOR(asampl);
+    DATA_VECTOR(temp);
+    DATA_IVECTOR(cat);
+    DATA_VECTOR(npp);
+    DATA_VECTOR(mesh);  
+    
+    PARAMETER(loga7);
+    PARAMETER_VECTOR(b5);
+    PARAMETER(logb1);
+    PARAMETER(logb2);
+    PARAMETER(logb3);
+    PARAMETER(logb4);
+    PARAMETER(logk);
+
+    Type a7=exp(loga7);
+    Type b1=exp(logb1);
+    Type b2=exp(logb2);
+    Type b3=exp(logb3);
+    Type b4=exp(logb4);
+    Type k=exp(logk);
+  
+    vector<Type> nu=exp(-b1/temp);
+    int nobs=nsp.size();
+    vector<Type> mu(nobs);
+    vector<Type> esiz=exp(siz);
+  
+    for(int i=0; i<nobs; ++i){
+      mu(i)=a7*nu(i)*pow(density(i),b2)*pow(esiz(i),b3)*pow(asampl(i),b4)*pow(mesh(i),b5(cat(i)))+Type(1.0e-12);
+    }
+  
+    vector<Type> var = mu + mu*mu/(k+Type(1.0e-12));
+    vector<Type> logL= dnbinom2(nsp,mu,var,true);
+    nll = -sum(vector<Type>(keep*logL));
+  
+    REPORT(mu);
+    REPORT(var);
+    REPORT(a7);
+    REPORT(b1);
+    REPORT(b2);
+    REPORT(b3);
+    REPORT(b4);
+    REPORT(b5);
+    REPORT(k);    
+    ADREPORT(a7);
+    ADREPORT(b1);
+    ADREPORT(b2);
+    ADREPORT(b3);
+    ADREPORT(b4);
+    ADREPORT(b5);
+    ADREPORT(k);
+    REPORT(nll);
+    REPORT(logL);
+  }
   return nll;
 }
-
